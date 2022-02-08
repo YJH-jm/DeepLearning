08fffc4ded7efc19bede91a2b0274aa48de27b81
YJH-jm <jmjhjob13@gmail.com>
2022-02-08 14:59:48 +0900

    실행파일 추가하여확인

--- steps/step17.py
+++ steps/step17.py
@@ -128,26 +128,7 @@ def exp(x):
 def add(x0, x1): # add 함수 구현 
     return Add()(x0,x1)
 
-def numerical_diff(f, x, eps = 1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-
-    return (y1.data - y0.data) / (2 * eps)
-
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-
-    return C(B(A(x)))
-
 if __name__ == "__main__":
-    x = Variable(np.array(2.0))
-    a = square(x)
-    y = add(square(a), square(a))
-    y.backward()
-
-    print(y.data)
-    print(x.grad)
+    for i in range(10):
+        x = Variable(np.random.randn(10000))  
+        y = square(square(square(x)))
