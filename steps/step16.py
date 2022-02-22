deda8def7d50d32ec036f0c3aa1479af5bb227de
YJH-jm <jmjhjob13@gmail.com>
2022-02-22 16:06:00 +0900

    Comment, Update

--- steps/step16.py
+++ steps/step16.py
@@ -43,7 +43,7 @@ class Variable:
             for x, gx in zip(f.inputs, gxs):
                 if x.grad is None:
                     x.grad = gx
-                    print("확인 : " ,x.grad)
+                    # print("확인 : " ,x.grad)
 
                 else:
                     x.grad = x.grad+gx                
@@ -63,6 +63,7 @@ def as_array(x):
 
 class Function:
     def __call__(self, *inputs): # 임의 개수의 인수 (가변 길이 인수)를 건네 받아 호출 가능 
+        # print("확인 : ", type(inputs)) # <class 'tuple'>
         xs = [x.data for x in inputs]
         ys = self.forward(*xs) # 리스트의 원소를 낱개로 풀어서 전송
 
@@ -97,6 +98,10 @@ class Square(Function):
         gx = 2 * x * gy
         return gx
 
+def square(x): 
+    f = Square()
+    return f(x)
+
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
@@ -106,6 +111,8 @@ class Exp(Function):
         gx = np.exp(x) * gy
         return gx
 
+def exp(x): 
+    return Exp()(x)
 
 class Add(Function):
     def forward(self, x0, x1):
@@ -116,31 +123,8 @@ class Add(Function):
     def backward(self,gy):
         return gy, gy
 
-def square(x): 
-    f = Square()
-    return f(x)
-
-def exp(x): 
-    return Exp()(x)
-
-
 def add(x0, x1): # add 함수 구현 
     return Add()(x0,x1)
-
-def numerical_diff(f, x, eps = 1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-
-    return (y1.data - y0.data) / (2 * eps)
-
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-
-    return C(B(A(x)))
 
 if __name__ == "__main__":
     x = Variable(np.array(2.0))
